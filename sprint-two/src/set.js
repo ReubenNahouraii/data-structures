44e63093be8511295e3aa15d8a0d8e0bfe8df050
GrokN <FullGrok@gmail.com>
2017-05-18 10:08:59 -0700

    Implement set function

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = undefined;
+
 };
 
 setPrototype.contains = function(item) {
+  let found = false;
+  _.each(this._storage, (value, prop) => {
+    if (item === prop) {
+      found = true;
+      return;
+    }
+  });
+  return found;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
