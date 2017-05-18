615c324805db35a4852c0df65dde3c3e59a3737d
GrokN <FullGrok@gmail.com>
2017-05-18 09:51:22 -0700

    Implement hashtable -- complete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,18 +7,23 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // if(this._storage.get(index))
-  this._storage.set(index, v);
+  let obj = this._storage.get(index);
+  if(!obj) {
+    obj  = {};
+  }
+
+  obj[k] = v;
+  this._storage.set(index, obj);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
+  this._storage.set(index, {});
 };
 
 
